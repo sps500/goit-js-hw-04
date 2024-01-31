429cb8a7a70386227a5e155414baed2d965b63f4
sps500 <148262907+sps500@users.noreply.github.com>
2024-01-31 20:16:37 +0200

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,19 @@
-function slugify(title) {
-  const slug = title.toLowerCase().replace(/\s+/g, "-");
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  // Обчислити загальну кількість товарів в об'єкті products
+  const totalProducts = Object.values(products).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+
+  // Перевірити, чи загальна кількість товарів менше або дорівнює containerSize
+  return totalProducts <= containerSize;
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// Перевірка результатів
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,25 @@
-function filterArray(numbers, value) {
-  const filteredArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredArray.push(numbers[i]);
-    }
-  }
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  // Метод для зміни імені профілю
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
+  // Метод для оновлення годин гри
+  updatePlayTime: function (hours) {
+    this.playTime += hours;
+  },
+  // Метод для отримання інформації
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-  return filteredArray;
-}
+// Перевірка результатів
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
